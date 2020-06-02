74f917dfa92a0aedc45dc9b85298092fdbd69dd8
lagiax <ivory_lagiax@hotmail.com>
2020-06-02 17:38:35 +0200

    Visual resources
    From png images to Unity source code

---
+++ Resources/CameraModeNames.cs
@@ -0,0 +1,22 @@
+﻿
+namespace CV.Resources {
+
+    public static class CameraModeNames {
+
+        public enum CameraModes {
+            CameraMode,
+            Fixed,
+            FixedTracking,
+            ClosestToTarget,
+            Chase,
+            ChaseFreeYawAxis,
+            Attached,
+            FirstPerson,
+            PlaneBinded,
+            PlanView,
+            RTS,
+            ThroughTarget,
+            FixedDirection
+        }
+    }
+}
